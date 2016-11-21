333d751cae662c3215a356c9ceb15c985f642382
ekoetsier <ernst.koetsier@sicpa.com>
2016-11-21 16:53:49 -0200

    Aula

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,13 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var formattedName = HTMLheaderName.replace("%data%","Ernst Koetsier");
+var formattedRole = HTMLheaderRole.replace("%data%","Developper");
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
+
+
+
+
+ 
